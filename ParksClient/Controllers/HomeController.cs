5763d1bc7aba10503a909465e0e0406aba5c52a0
ChrisRDavila <crossdavila@gmail.com>
2023-06-09 16:24:02 -0700

    Home controller updated

--- ParksClient/Controllers/HomeController.cs
+++ ParksClient/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using ParksClient.Models;
 
+namespace ParksClient.Controllers;
 
-
-namespace ProjectName.Controllers
+public class HomeController : Controller
 {
-  public class HomeController : Controller
-  {
-    [Route("/")]
-    // Notice the changes below!
-    public ActionResult File() { return View(); }
-  }
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
